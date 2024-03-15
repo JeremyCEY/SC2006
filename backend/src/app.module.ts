97c242b56fa53275325ac0c741a320fe22e8a0b5
WJ-MJ <115694501+WJ-MJ@users.noreply.github.com>
2024-03-15 15:19:01 +0800

    create backend environment
    create backend server and link to database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ResaleModule } from './resale/resale.module';
+
+const url = 'mongodb+srv://tanw0272:UVdMb8aeATcDmYuc@sc2006.fa1n11l.mongodb.net/ResaleHDBInfo?retryWrites=true&w=majority&appName=SC2006';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TasksModule, ResaleModule, MongooseModule.forRoot(url)]
 })
 export class AppModule {}
