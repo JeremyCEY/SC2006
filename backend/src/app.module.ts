5c4736b51829bc50ff887ec53566f92675a45ffa
WJ-MJ <115694501+WJ-MJ@users.noreply.github.com>
2024-03-28 16:48:19 +0800

    update code

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ResaleModule } from './resale/resale.module';
 import { AuthModule } from './auth/auth.module';
@@ -8,6 +7,6 @@ import { TestDataModule } from './testData/testData.module';
 const url = 'mongodb+srv://tanw0272:UVdMb8aeATcDmYuc@sc2006.fa1n11l.mongodb.net/ResaleHDBInfo?retryWrites=true&w=majority&appName=SC2006';
 
 @Module({
-  imports: [TasksModule, ResaleModule, TestDataModule, MongooseModule.forRoot(url), AuthModule]
+  imports: [ResaleModule, TestDataModule, MongooseModule.forRoot(url), AuthModule]
 })
 export class AppModule {}
