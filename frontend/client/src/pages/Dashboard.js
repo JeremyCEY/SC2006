1717470ffcd73d40ea20e20331e23a7b8600ff0e
McSpicyWifBacon <chowenyao@gmail.com>
2024-04-01 20:04:51 +0800

    cleaned up code and added dynamic rendering in dashboard

--- frontend/client/src/pages/Dashboard.js
+++ frontend/client/src/pages/Dashboard.js
@@ -14,6 +14,12 @@ function Dashboard() {
 
 
     {/* Sidebar Selection */}
+
+    const handleMenuClick = (key) => {
+        setActiveSection(key);
+    };
+
+
     const renderContent = () => {
         switch (activeSection) {
             case 'savedProperties':
@@ -43,13 +49,14 @@ function Dashboard() {
                         defaultSelectedKeys={['saved_properties']}
                         className='h-full border-0 bg-gray-50 font-semibold text-1xl'
                         style={{ height: '100%', borderRight: 0 }}
+                        onClick={({ key }) => handleMenuClick(key)}
                         items = {[
                             {
-                                key: 'saved_properties',
+                                key: 'savedProperties',
                                 label: 'Saved Properties',
                             },
                             {
-                                key: 'frequently_visited',
+                                key: 'frequentlyVisited',
                                 label: 'Frequently Visited',
                             },
                             {
@@ -60,47 +67,9 @@ function Dashboard() {
                         ]}
                     />
                 </Sider>
-                {/* <aside 
-                    className={`bottom-0 z-30 w-64 transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} sm:translate-x-0 transition-transform bg-gray-50 bg-gray-300 overflow-y-auto`}
-                    style={{ top: navbarHeight }} 
-                >
-                    <ul className="space-y-2 p-4 font-medium">
-                        <li>
-                            <button
-                                onClick={() => setActiveSection('savedProperties')}
-                                className="flex items-center p-2 text-black-900 hover:text-white rounded-lg hover:bg-blue-700 w-full text-left"
-                            >
-                                Saved Properties
-                            </button>
-                        </li>
-                        <li>
-                            <button
-                                onClick={() => setActiveSection('frequentlyVisited')}
-                                className="flex items-center p-2 text-black-900 hover:text-white rounded-lg hover:bg-blue-700 w-full text-left"
-                            >
-                                Frequently Visited
-                            </button>
-                        </li>
-                        <li>
-                            <button
-                                onClick={() => setActiveSection('settings')}
-                                className="flex items-center p-2 text-black-900 hover:text-white rounded-lg hover:bg-blue-700 w-full text-left"
-                            >
-                                Settings
-                            </button>
-                        </li>
-                    </ul>
-                </aside>
-                <button 
-                    onClick={() => setIsSidebarOpen(!isSidebarOpen)} 
-                    className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                    aria-controls="default-sidebar"
-                    data-drawer-toggle="default-sidebar"
-                    type="button"
-                ></button> */}
 
                 {/* Page content */}
-                <div className="flex-1 pl-64">
+                <div className="flex">
                     {renderContent()}
                 </div>
             </div>
