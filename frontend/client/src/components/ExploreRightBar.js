810f739b98972c9ea4174fead6aa908b7d297a52
McSpicyWifBacon <chowenyao@gmail.com>
2024-04-04 00:01:58 +0800

    extracted components in explore.js

---
+++ frontend/client/src/components/ExploreRightBar.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+
+import { Layout, Button, ConfigProvider } from 'antd';
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
+
+const { Sider } = Layout;
+
+
+
+function ExploreRightBar({ isAuthenticated, frequentAddresses }) {
+    
+    const [collapsedRight, setCollapsedRight] = useState(false);
+
+    const onCollapseRight = collapsedRight => {
+        setCollapsedRight(collapsedRight);
+    };
+
+
+    const toggleSidebarRight = () => {
+        setCollapsedRight(!collapsedRight);
+    };
+    
+    
+    
+    return (
+        <>
+        {isAuthenticated && (
+        <>
+            <Sider
+                collapsible
+                collapsed={collapsedRight}
+                onCollapse={onCollapseRight}
+                collapsedWidth={0}
+                width={400}
+                trigger={null}
+                style={{
+                    position: 'absolute',
+                    right: 0,
+                    top: '13vh',
+                    zIndex: 100,
+                    height: '40vh',
+                    backgroundColor: 'white',
+                }}
+            >
+                {/* Render Frequently Visited Locations under the user account */}
+                <div>
+                {frequentAddresses.map((address, index) => (
+                    <div key={index} className="flex items-center justify-between p-4 border-b border-gray-300">
+                        <div className="flex-grow">
+                            <p className="text-lg font-semibold text-blue-600">{address}</p>
+                        </div>
+                    </div>
+                ))}
+                </div>
+            </Sider>
+                
+
+
+            <ConfigProvider wave={{ disabled: true }}>
+            <Button
+                type="primary"
+                icon={collapsedRight ? <LeftOutlined className='text-black'/> : <RightOutlined className='text-black'/>}
+                loading={false} // Ensure loading state is off
+                onClick={toggleSidebarRight}
+                style={{
+                    position: 'absolute',
+                    right: collapsedRight ? 0 : '400px',
+                    transition: 'right 0.2s ease',
+                    zIndex: 10,
+                    top: '30vh',
+                    backgroundColor: 'white',
+                    borderRadius: '0%',
+                    height: '50px',
+                    border: 'none',
+                    boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)'
+                }}
+            />
+            </ConfigProvider>
+        </>
+        )}
+        </>
+    )
+}
+export default ExploreRightBar;
--- frontend/client/src/pages/Explore.js
+++ frontend/client/src/pages/Explore.js
@@ -1,18 +1,17 @@
-//Explore.js
-
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
 
-import { Layout, Menu, Button, ConfigProvider, List, Select } from 'antd';
-import { LeftOutlined, RightOutlined, FilterOutlined, ArrowUpOutlined, ArrowDownOutlined } from '@ant-design/icons';
+import { Layout } from 'antd';
 
 
 import LoggedInNavbar from '../components/LoggedInNavbar';
 import LoggedOutNavbar from "../components/LoggedOutNavbar";
 import Map from '../components/Map';
+import SearchResultsBar from '../components/SearchResultsBar';
+import ExploreRightBar from '../components/ExploreRightBar';
 
-
-const { Sider, Content } = Layout;
+const { Content } = Layout;
 
 
 function Explore(){
@@ -24,15 +23,6 @@ function Explore(){
         setIsAuthenticated(token !== null);
     }, []);
 
-    const [collapsed, setCollapsed] = useState(false);
-
-    const onCollapse = collapsed => {
-        setCollapsed(collapsed);
-    };
-
-    const toggleSidebar = () => {
-        setCollapsed(!collapsed);
-    };
     
     const location = useLocation();
     const responseData = location.state.responseData;
@@ -71,6 +61,42 @@ function Explore(){
         setSortedData(sorted);
     }, [responseData, sortOption]);
 
+
+    const [frequentAddresses, setFrequentAddresses] = useState([]);
+    const [userId, setUserId] = useState(null); // State for user ID
+
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            const decodedToken = jwtDecode(token);
+            setUserId(decodedToken.id); // Set user ID from decoded token
+        }
+        if (userId) {
+            fetch(`http://localhost:3000/frequentaddress/${userId}`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                },
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                // console.log('response:', response);
+                return response.json();
+            })
+            .then(data => {
+                console.log('Frequent addresses:', data);
+                setFrequentAddresses(data); 
+            })
+            .catch(error => {
+                console.error('Error fetching frequent addresses:', error);
+            });
+        }
+    }, [userId]);  
+
     return(
         <>
             <div className='relative z-[1000]'>
@@ -86,100 +112,13 @@ function Explore(){
                         />                    
                     </Content>
                 </Layout>
+            
 
+                <SearchResultsBar setSortOption={setSortOption} sortedData={sortedData} handleDivClick={handleDivClick} />
                 
-                <Sider
-                    collapsible
-                    collapsed={collapsed}
-                    onCollapse={onCollapse}
-                    collapsedWidth={0}
-                    width={500}
-                    trigger={null}
-                    style={{
-                        position: 'absolute',
-                        left: 0,
-                        zIndex: 100,
-                        height: '87vh',
-                        backgroundColor: 'white',
-                    }}
-                >
-                    <div className="justify-between flex mt-5 mb-5">
-                        <span className="font-bold text-3xl ml-10" >Search Results</span>
-                        
+                
+                <ExploreRightBar isAuthenticated={isAuthenticated} frequentAddresses={frequentAddresses} />                
 
-                        <Select
-                            suffixIcon={<FilterOutlined />}
-                            defaultValue="Sort By"
-                            className="bg-white border-none text-black shadow-md mr-10"
-                            onChange={(value) => {
-                                setSortOption(value);
-                            }}
-
-                        >
-                            <Select.Option  value="price up">Price <ArrowUpOutlined/></Select.Option>
-                            <Select.Option  value="price down">Price <ArrowDownOutlined/></Select.Option>
-
-                            <Select.Option value="size up">Size <ArrowUpOutlined/></Select.Option>
-                            <Select.Option value="size down">Size <ArrowDownOutlined/></Select.Option>
-
-                        </Select>
-                    </div>
-                    
-       
-                    
-                    
-                    <div className='h-[77vh] overflow-auto justify-center flex'>
-                        <List className='ml-4'
-                            grid={{ column: 2 }} // Adjust column count as needed
-                            
-                            // dataSource={responseData}
-                            dataSource={sortedData}
-
-                            renderItem={resale => (
-                                <List.Item>
-                                    <Button className=" h-[20vh] w-[15vw]  shadow-md bg-gray-50"
-                                        key={resale.id}
-                                        onClick={() => handleDivClick(resale)} // Add this line
-
-                                    >
-                                        <div className="header">
-                                            <div className="residence-name">{resale.town}</div>
-                                            <div className="price-range">${resale.resale_price.toLocaleString()}</div>
-                                        </div>
-                                        <ul className="residence-details">
-                                            <li>Type: {resale.flat_type}</li>
-                                            <li>Street: {resale.street_name}</li>
-                                            <li>Floor Area: {resale.floor_area_sqm} sqm</li>
-                                        </ul>
-                                    </Button>
-                                </List.Item>
-                            )}
-                            
-                        />
-                    </div>
-
-                </Sider>
-
-                <ConfigProvider wave={{ disabled: true }}>
-                <Button
-                    type="primary"
-                    icon={collapsed ? <RightOutlined className='text-black'/> : <LeftOutlined className='text-black'/>}
-                    loading={false} // Ensure loading state is off
-                    onClick={toggleSidebar}
-                    style={{
-                        position: 'absolute',
-                        left: collapsed ? 0 : '500px',
-                        transition: 'left 0.2s ease',
-                        zIndex: 1001,
-                        top: '50vh',
-                        backgroundColor: 'white',
-                        borderRadius: '0%',
-                        height: '50px',
-                        border: 'none',
-                        boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)'
-                    }}
-                />
-                </ConfigProvider>
             </Layout>
             
         </>
