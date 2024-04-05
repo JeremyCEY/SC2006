6887b0efed5f3c90ac2ca8b815794cebbc00d701
boonyii <laiboonyi@hotmail.com>
2024-04-05 13:34:24 +0800

    minor change

--- backend/src/auth/dto/signup.dto.ts
+++ backend/src/auth/dto/signup.dto.ts
@@ -6,7 +6,7 @@ export class SignUpDto {
   readonly name: string;
 
   @IsNotEmpty()
-  @IsEmail({}, { message: 'Please enter correct email' })
+  @IsEmail({}, { message: 'Please enter valid email' })
   readonly email: string;
 
   @IsNotEmpty()
